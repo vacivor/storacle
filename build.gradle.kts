7c9161baa3754816aa958bd0afd6f0c8f466e9f5
vacivor <vacivor@gmail.com>
2026-03-15 14:16:05 +0800

    chore: upgrade Apache Tika to 3.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ val huaweiObsVersion = "3.25.10"
 val googleCloudStorageVersion = "2.62.1"
 val baiduBosVersion = "0.10.412"
 val aliyunOssVersion = "3.18.5"
-val tikaVersion = "2.9.2"
+val tikaVersion = "3.2.3"
 
 extra["springBootVersion"] = springBootVersion
 extra["awsSdkVersion"] = awsSdkVersion
